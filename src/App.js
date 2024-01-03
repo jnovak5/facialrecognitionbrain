e22616b8d9c1369ef85290c240e4e5aae74d4cb6
Jonathon Novak <jcnovak5@gmail.com>
2024-01-02 18:37:05 -0600

    Components Created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import tachyons from 'tachyons';
+import ParticlesBg from 'particles-bg'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParticlesBg className="patricles" color="#FFFFFF" num={30} type="cobweb" bg={true} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*}
+      <FaceRecognition />*/}
     </div>
   );
 }
