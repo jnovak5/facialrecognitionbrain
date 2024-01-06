78b31bd0e8ae71a6b1a13a0318c42d7bc2432ea1
Jonathon Novak <jcnovak5@gmail.com>
2024-01-05 20:18:10 -0600

    API Integrated

--- src/App.js
+++ src/App.js
@@ -3,20 +3,78 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import tachyons from 'tachyons';
 import ParticlesBg from 'particles-bg'
+import React, { useState } from 'react';
+
+
+const returnClarifaiJSONRequest = (imageUrl) => {
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = 'eedffe04cb79463a8cf54fceef841bb5';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'jcnovak5';       
+    const APP_ID = 'face-recognition-brain';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const IMAGE_URL = imageUrl;
+    
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+    
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    return requestOptions;
+}
 
 
 function App() {
+
+  const [url,setUrl] = useState('')
+
+  const onInputChange = (event) => {
+    setUrl(event.target.value);
+  }
+
+
+  const onButtonSubmit = () => {
+    console.log('Click');
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiJSONRequest(url))
+    .then(response => response.json())
+    .then(response => {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    })
+  }
+
   return (
     <div className="App">
-      <ParticlesBg className="patricles" color="#FFFFFF" num={30} type="cobweb" bg={true} />
+      <ParticlesBg className="patricles" color="#FFFFFF" num={200} type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/*}
-      <FaceRecognition />*/}
+      <ImageLinkForm onInputChange={onInputChange} onButtonSubmit={onButtonSubmit} />
+      <FaceRecognition imageUrl={url} />
     </div>
   );
 }
